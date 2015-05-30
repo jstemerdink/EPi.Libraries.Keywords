de82598e26c73724a56f3c24ea4d72039d7c9c77
Jeroen <jeroen.developer@outlook.com>
2015-05-30 17:59:47 +0200

    Added Gitversion

--- EPi.Libraries.Keywords.Alchemy/Properties/AssemblyInfo.cs
+++ EPi.Libraries.Keywords.Alchemy/Properties/AssemblyInfo.cs
@@ -49,16 +49,3 @@ using log4net.Config;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("2378cd81-b5bc-4826-80e0-74d2ed1febd4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 By Jeroen Stemerdink
 
 [![Build status](https://ci.appveyor.com/api/projects/status/tt6pwtrm8k3k2gf9/branch/master?svg=true)](https://ci.appveyor.com/project/jstemerdink/epi-libraries-keywords/branch/master)
+[![GitHub version](https://badge.fury.io/gh/jstemerdink%2FEPi.Libraries.Keywords.svg)](http://badge.fury.io/gh/jstemerdink%2FEPi.Libraries.Keywords)
 
 ## Parts
 
